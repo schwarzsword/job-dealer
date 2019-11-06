6204cc793cb1fc44a1f844ddbadcc8a9b5e21572
egnaf <mamadaliev@yahoo.com>
2019-11-06 10:33:01 +0300

    Fix Vacancy

--- backend/src/main/java/edu/netcracker/jobdealer/service/impl/VacancyServiceImpl.java
+++ backend/src/main/java/edu/netcracker/jobdealer/service/impl/VacancyServiceImpl.java
@@ -1,85 +1,37 @@
 package edu.netcracker.jobdealer.service.impl;
 
 import edu.netcracker.jobdealer.dto.VacancyDto;
-import edu.netcracker.jobdealer.entity.Company;
-import edu.netcracker.jobdealer.entity.Vacancy;
-import edu.netcracker.jobdealer.exceptions.VacancyNotFoundException;
 import edu.netcracker.jobdealer.repository.VacancyRepository;
 import edu.netcracker.jobdealer.service.VacancyService;
+import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-@Service("vacancyService")
-@Transactional
+@Service
 public class VacancyServiceImpl implements VacancyService {
 
-    private final VacancyRepository vacancyRepository;
-
+    private VacancyRepository vacancyRepository;
+    private Mapper mapper;
 
     @Autowired
-    public VacancyServiceImpl(final VacancyRepository vacancyRepository) {
+    public VacancyServiceImpl(VacancyRepository vacancyRepository, Mapper mapper) {
         this.vacancyRepository = vacancyRepository;
+        this.mapper = mapper;
     }
 
     @Override
-    public List<Vacancy> getAll() {
-        return vacancyRepository.findAll();
+    public List<VacancyDto> getAllVacancies() {
+        return vacancyRepository.findAll().stream()
+                .map(vacancy -> mapper.map(vacancy, VacancyDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public List<Vacancy> getVacanciesByCompany(Company company) {
-        return vacancyRepository.findByOwner(company);
+    public VacancyDto getVacancyById(UUID id) {
+        return mapper.map(vacancyRepository.getOne(id), VacancyDto.class);
     }
-
-    @Override
-    public Vacancy addVacancy(Vacancy vacancy) {
-
-        Vacancy vacancyToAdd = new Vacancy(
-                vacancy.getName(),
-                vacancy.getDescription(),
-                vacancy.getMoney(),
-                vacancy.getRequestedSkills(),
-                vacancy.getOwner()
-        );
-
-        return vacancyRepository.saveAndFlush(vacancyToAdd);
-    }
-
-    @Override
-    public void remove(UUID vacancyId) throws VacancyNotFoundException{
-        Optional<Vacancy> byId = vacancyRepository.findById(vacancyId);
-        if (byId.isPresent()) {
-            vacancyRepository.delete(byId.get());
-        } else throw new VacancyNotFoundException("Vacancy not found");
-    }
-
-
-    @Override
-    public Vacancy updateVacancy(VacancyDto vacancyDTO) {
-        Vacancy vacancyToUpdate = vacancyRepository.findByNameAndOwner(vacancyDTO.getName(), vacancyDTO.getOwner());
-
-        if (vacancyDTO.getDescription() != null) {
-            vacancyToUpdate.setDescription(vacancyDTO.getDescription());
-        }
-        if (vacancyDTO.getMoney() != null) {
-            vacancyToUpdate.setMoney(vacancyDTO.getMoney());
-        }
-        if (vacancyDTO.getName() != null) {
-            vacancyToUpdate.setName(vacancyDTO.getName());
-        }
-        if (vacancyDTO.getOwner() != null) {
-            vacancyToUpdate.setOwner(vacancyDTO.getOwner());
-        }
-        if (vacancyDTO.getRequestedSkills() != null) {
-            vacancyToUpdate.setRequestedSkills(vacancyDTO.getRequestedSkills());
-        }
-
-        return vacancyRepository.saveAndFlush(vacancyToUpdate);
-    }
-
 }
