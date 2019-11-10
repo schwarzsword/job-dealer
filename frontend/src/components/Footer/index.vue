73f34e2ffa2ec03bf9865b89362ef6500c716238
egnaf <mamadaliev@yahoo.com>
2019-11-10 16:16:03 +0300

    Add footer component

---
+++ frontend/src/components/Footer/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer">
+        <div class="container">
+            <router-link class="nav-link" to="/about">About</router-link>
+            <router-link class="nav-link" to="/rules">Rules</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footer',
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style>
+    .footer a {
+        float: left;
+        font-size: 14px;
+    }
+</style>
