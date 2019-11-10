01bd7e3d58534bb0055e1413f9c8c069ffead6e0
Kirill <dankirill1997@mail.ru>
2019-11-10 19:31:22 +0300

    add data source configuration

---
+++ backend/src/main/java/edu/netcracker/jobdealer/controller/test/testConfiguration/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package edu.netcracker.jobdealer.controller.test.testConfiguration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:test");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+}
