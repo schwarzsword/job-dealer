23fc632b55d32bd4feb10ff52355f28e41fc65dd
egnaf <mamadaliev@yahoo.com>
2019-11-11 19:06:52 +0300

    Fix company service and controller

--- backend/src/main/java/edu/netcracker/jobdealer/controller/CompanyController.java
+++ backend/src/main/java/edu/netcracker/jobdealer/controller/CompanyController.java
@@ -1,12 +1,14 @@
 package edu.netcracker.jobdealer.controller;
 
 import edu.netcracker.jobdealer.dto.CompanyDto;
+import edu.netcracker.jobdealer.exceptions.AccountIdAlreadyExistsException;
+import edu.netcracker.jobdealer.exceptions.CompanyNotFoundException;
 import edu.netcracker.jobdealer.service.CompanyService;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -34,5 +36,41 @@ public class CompanyController {
     @GetMapping(value = "/companies/{id}")
     public CompanyDto getCompanyById(@PathVariable("id") UUID id) {
         return mapper.map(companyService.getCompanyById(id), CompanyDto.class);
+    }
+
+    @PostMapping(value = "/companies")
+    public ResponseEntity<?> addCompany(@RequestParam String name, @RequestParam Boolean isVerified,
+                                 @RequestParam String description, @RequestParam String avatarUrl,
+                                 @RequestParam UUID accountId) {
+        try {
+            CompanyDto company = mapper.map(
+                    companyService.addCompany(name, isVerified, description, avatarUrl, accountId), CompanyDto.class);
+            return new ResponseEntity<>(company, HttpStatus.OK);
+        } catch (AccountIdAlreadyExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping(value = "/companies")
+    public ResponseEntity<?> updateCompany(@RequestParam UUID id, @RequestParam String name,
+                                           @RequestParam Boolean isVerified, @RequestParam String description,
+                                           @RequestParam String avatarUrl,
+                                           @RequestParam UUID accountId) {
+        try {
+            CompanyDto company = mapper.map(
+                    companyService.updateCompany(id, name, isVerified, description, avatarUrl, accountId), CompanyDto.class);
+            return new ResponseEntity<>(company, HttpStatus.OK);
+        } catch (AccountIdAlreadyExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(value = "/companies")
+    public ResponseEntity deleteCompany(@RequestParam String id) {
+        try {
+            return companyService.deleteCompany(UUID.fromString(id));
+        } catch (CompanyNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
