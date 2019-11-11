475bb3ca816765edd82b3e867beaa7a254039514
egnaf <mamadaliev@yahoo.com>
2019-11-11 14:36:41 +0300

    Fix constructors with parameters for Company

--- backend/src/main/java/edu/netcracker/jobdealer/entity/Company.java
+++ backend/src/main/java/edu/netcracker/jobdealer/entity/Company.java
@@ -38,6 +38,23 @@ public class Company {
     @OneToMany
     private List<Vacancy> vacancies;
 
+    public Company(UUID id, String name, Boolean verified, String description, String avatarUrl, UUID accountId) {
+        this.id = id;
+        this.name = name;
+        this.verified = verified;
+        this.description = description;
+        this.avatarUrl = avatarUrl;
+        this.account.setId(accountId);
+    }
+
+    public Company(String name, Boolean verified, String description, String avatarUrl, UUID accountId) {
+        this.name = name;
+        this.verified = verified;
+        this.description = description;
+        this.avatarUrl = avatarUrl;
+        this.account.setId(accountId);
+    }
+
     @Mapping("accountId")
     public UUID getAccountId() {
         return this.account.getId();
