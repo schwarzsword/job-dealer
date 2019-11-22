92df76bbfc2632b9e736dce44a05c9444fe3a17e
egnaf <mamadaliev@yahoo.com>
2019-11-22 16:23:11 +0300

    Fix message not found exception

--- backend/src/main/java/edu/netcracker/jobdealer/exceptions/MessageNotFoundException.java
+++ backend/src/main/java/edu/netcracker/jobdealer/exceptions/MessageNotFoundException.java
@@ -1,7 +1,12 @@
 package edu.netcracker.jobdealer.exceptions;
 
 public class MessageNotFoundException extends NotFoundException {
+
     public MessageNotFoundException() {
         super("Message not found");
+    }
+
+    public MessageNotFoundException(String msg) {
+        super(msg);
     }
 }
