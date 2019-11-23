af205ebe4de9d5a7814be656ba9127bfe93f51a6
egnaf <mamadaliev@yahoo.com>
2019-11-23 22:33:25 +0300

    Fix message controller

--- backend/src/main/java/edu/netcracker/jobdealer/controller/MessageController.java
+++ backend/src/main/java/edu/netcracker/jobdealer/controller/MessageController.java
@@ -2,89 +2,63 @@ package edu.netcracker.jobdealer.controller;
 
 import edu.netcracker.jobdealer.dto.MessageDto;
 import edu.netcracker.jobdealer.entity.Message;
-import edu.netcracker.jobdealer.exceptions.NotFoundException;
 import edu.netcracker.jobdealer.service.MessageService;
+import lombok.extern.slf4j.Slf4j;
 import org.dozer.Mapper;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 public class MessageController {
 
+    private final MessageService messageService;
     private final Mapper mapper;
 
-    private final MessageService messageService;
-
+    @Autowired
     public MessageController(MessageService messageService, Mapper mapper) {
         this.messageService = messageService;
         this.mapper = mapper;
     }
 
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping(value = "my/messages")
-    public ResponseEntity<?> getUserMessages(@AuthenticationPrincipal User user) {
-        List<Message> userMessages = messageService.getUserMessages(user.getUsername());
-        List<MessageDto> dtos = userMessages
-                .stream()
-                .map(e -> mapper.map(e, MessageDto.class))
+    @GetMapping("/messages")
+    public List<MessageDto> getMessages(@PathVariable UUID senderId, @PathVariable UUID receiverId,
+                                        @PathVariable int offset, @PathVariable int limit) {
+        List<MessageDto> messages = messageService.getMessages(senderId, receiverId, offset, limit).stream()
+                .map(message -> mapper.map(message, MessageDto.class))
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(dtos);
+        log.debug(messages.toString());
+        return messages;
     }
 
-
-    @PreAuthorize("isAuthenticated()")
-    @PostMapping(value = "/accounts/{id}/messages")
-    public ResponseEntity<?> sendMessage(@PathVariable("id") UUID receiver,
-                                         @RequestParam String text,
-                                         @AuthenticationPrincipal User user) {
-        try {
-            messageService.sendMessage(text, user.getUsername(), receiver);
-        } catch (NotFoundException ex) {
-            return ResponseEntity.status(404).body(ex.getMessage());
-
-        }
-        return ResponseEntity.noContent().build();
+    @GetMapping("/messages/{id}")
+    public MessageDto getMessage(@PathVariable UUID id) {
+        MessageDto message =  mapper.map(messageService.getMessage(id), MessageDto.class);
+        log.debug(message.toString());
+        return message;
     }
 
+    @PostMapping("/messages")
+    public MessageDto sendMessage(@RequestBody Message msg) {
+        MessageDto message = mapper.map(messageService.sendMessage(msg), MessageDto.class);
+        log.debug(message.toString());
+        return message;
+    }
 
-    //May be this is not necessary
+    @PutMapping("/message")
+    public MessageDto updateMessage(@RequestParam UUID id, @RequestParam String text) {
+        MessageDto message = mapper.map(messageService.updateMessage(id, text), MessageDto.class);
+        log.debug(message.toString());
+        return message;
+    }
 
-//    @PreAuthorize("isAuthenticated()")
-//    @GetMapping(value = "{email}/messages/{mesId}")
-//    public ResponseEntity getMessage(@PathVariable("email") String email,
-//                                     @PathVariable("mesId") UUID mesId,
-//                                     @AuthenticationPrincipal User user) {
-//        Message message = messageService.getMessage(mesId);
-//        MessageDto messageDto = mapper.map(message, MessageDto.class);
-//        try {
-//            if (message.getMessageDest().getEmail().equals(user.getUsername())) {
-//                return ResponseEntity.ok(messageDto);
-//            } else return ResponseEntity.badRequest().body("You have no permission to read this message");
-//        } catch (MessageNotFoundException e) {
-//            return ResponseEntity.badRequest().body("No message found");
-//        }
-//    }
-
-
-//    @PreAuthorize("isAuthenticated()")
-//    @DeleteMapping(value = "{email}/messages/{mesId}")
-//    public ResponseEntity<?> deleteMessage(@PathVariable("email") String email,
-//                                           @PathVariable("mesId") UUID mesId,
-//                                           @AuthenticationPrincipal User user) {
-//        try {
-//            messageService.deleteMessage(mesId, user.getUsername());
-//        } catch (NoPermissionException ex) {
-//            return ResponseEntity.badRequest().body("You have no permission to delete this message");
-//        } catch (MessageNotFoundException e) {
-//            return ResponseEntity.badRequest().body("No message found");
-//        }
-//        return ResponseEntity.noContent().build();
-//    }
+    @DeleteMapping("/users/{id}")
+    public void deleteMessage(@PathVariable UUID id) {
+        messageService.deleteMessage(id);
+        log.debug("User by id=" + id + " was deleted");
+    }
 }
