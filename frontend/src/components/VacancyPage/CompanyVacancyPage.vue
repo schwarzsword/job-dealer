1f7da800f24fcb3e81e4e79b3111a0edf7888eaa
egnaf <mamadaliev@yahoo.com>
2019-12-17 20:10:38 +0300

    Fix company vacancy page styles

--- frontend/src/components/VacancyPage/CompanyVacancyPage.vue
+++ frontend/src/components/VacancyPage/CompanyVacancyPage.vue
@@ -1,145 +1,153 @@
 <template>
-    <v-app>
+  <div>
+    <div>
+      <div class="left-sidebar" style="text-align: center; margin: 10px 0 0 0;">
+      </div>
+      <div class="content">
         <v-card>
-            {{vacancy.name}}
-            {{vacancy.description}}
-            {{vacancy.money}}
-            {{vacancy.ownerName}}
-            <ul>
-                <li v-for="skill in vacancy.requestedSkills">
-                    {{ skill }}
-                </li>
-            </ul>
+          {{vacancy.name}}
+          {{vacancy.description}}
+          {{vacancy.money}}
+          {{vacancy.ownerName}}
+          <ul>
+            <li v-for="skill in vacancy.requestedSkills">
+              {{ skill }}
+            </li>
+          </ul>
         </v-card>
         <v-card v-if="task.name">
-            {{task.name}}
-            {{task.description}}
+          {{task.name}}
+          {{task.description}}
         </v-card>
 
         <v-data-table
-                :headers="headers"
-                :items="responses"
-                class="elevation-1"
-                sort-by="calories"
+            :headers="headers"
+            :items="responses"
+            class="elevation-1"
+            sort-by="calories"
         >
-            <template v-slot:top>
-                <v-toolbar color="white" flat>
-                    <v-toolbar-title>Responces</v-toolbar-title>
-                </v-toolbar>
-            </template>
-            <template v-slot:item.action="{ item }">
+          <template v-slot:top>
+            <v-toolbar color="white" flat>
+              <v-toolbar-title>Responces</v-toolbar-title>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.action="{ item }">
 
-                <v-icon
-                        @click="downloadSubmission(item)"
-                >
-                    mdi-cloud-download-outline
-                </v-icon>
+            <v-icon
+                @click="downloadSubmission(item)"
+            >
+              mdi-cloud-download-outline
+            </v-icon>
 
-                <v-icon @click="accept(item)"
-                        v-if="item.status === 'APPLIED'"
-                >
-                    mdi-check-outline
-                </v-icon>
+            <v-icon @click="accept(item)"
+                    v-if="item.status === 'APPLIED'"
+            >
+              mdi-check-outline
+            </v-icon>
 
-                <v-icon
-                        @click="reject(item)"
-                        v-if="item.status === 'APPLIED'"
-                >
-                    mdi-close-outline
-                </v-icon>
-                <v-icon
-                        @click="route(item)"
-                >
-                    mdi-account-badge-horizontal-outline
-                </v-icon>
-            </template>
-            <template v-slot:no-data>
-                You have no active responses
-            </template>
+            <v-icon
+                @click="reject(item)"
+                v-if="item.status === 'APPLIED'"
+            >
+              mdi-close-outline
+            </v-icon>
+            <v-icon
+                @click="route(item)"
+            >
+              mdi-account-badge-horizontal-outline
+            </v-icon>
+          </template>
+          <template v-slot:no-data>
+            You have no active responses
+          </template>
         </v-data-table>
-    </v-app>
+      </div>
+      <div class="right-sidebar">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
-    import Router from "../../router"
-    import {urlPort} from "../../tool";
-    import download from "downloadjs"
+  import Router from "../../router"
+  import {urlPort} from "../../tool";
+  import download from "downloadjs"
 
-    export default {
-        name: "companyVacancyPage",
-        data: function () {
-            return {
-                fileBytes: null,
-                vacancy: {
-                    id: Router.currentRoute.params.id,
-                    name: "",
-                    description: "",
-                    money: null,
-                    ownerName: "",
-                    requestedSkills: [],
-                    withTask: null,
-                },
-                task: {
-                    id: null,
-                    name: "",
-                    description: "",
-                },
-                responses: [],
-                submissions: [],
-                headers: [
-                    {text: 'Applicant name', value: 'applicantName',},
-                    {text: 'Status', value: 'status',},
-                    {text: 'Actions', value: 'action', sortable: false},
-                ],
-            }
+  export default {
+    name: "companyVacancyPage",
+    data: function () {
+      return {
+        fileBytes: null,
+        vacancy: {
+          id: Router.currentRoute.params.id,
+          name: "",
+          description: "",
+          money: null,
+          ownerName: "",
+          requestedSkills: [],
+          withTask: null,
         },
-        created() {
-            urlPort.get("/vacancies/" + this.vacancy.id)
-                .then(resp => {
-                    this.vacancy = resp.data;
-                    urlPort.get("/my/vacancies/" + this.vacancy.id)
-                        .then(resp => {
-                            this.responses = resp.data
-                        });
-                    if (this.vacancy.withTask) {
-                        urlPort.get("/vacancies/" + this.vacancy.id + "/tasks")
-                            .then(resp => {
-                                this.task = resp.data;
-                                urlPort.get("/vacancies/" + this.vacancy.id + "/tasks/" + this.task.id)
-                                    .then(resp => {
-                                        this.submissions = resp.data;
-                                    })
-                            });
+        task: {
+          id: null,
+          name: "",
+          description: "",
+        },
+        responses: [],
+        submissions: [],
+        headers: [
+          {text: 'Applicant name', value: 'applicantName',},
+          {text: 'Status', value: 'status',},
+          {text: 'Actions', value: 'action', sortable: false},
+        ],
+      }
+    },
+    created() {
+      urlPort.get("/vacancies/" + this.vacancy.id)
+        .then(resp => {
+          this.vacancy = resp.data;
+          urlPort.get("/my/vacancies/" + this.vacancy.id)
+            .then(resp => {
+              this.responses = resp.data
+            });
+          if (this.vacancy.withTask) {
+            urlPort.get("/vacancies/" + this.vacancy.id + "/tasks")
+              .then(resp => {
+                this.task = resp.data;
+                urlPort.get("/vacancies/" + this.vacancy.id + "/tasks/" + this.task.id)
+                  .then(resp => {
+                    this.submissions = resp.data;
+                  })
+              });
 
-                    }
-                });
-        },
-        methods: {
-            accept(item) {
-                item.status = "INVITED";
-                this.setStatus(item);
-            },
-            reject(item) {
-                item.status = "REJECTED";
-                this.setStatus(item);
-            },
-            route(item) {
-                //todo: сделать линк на резюме пользователя
-            },
-            setStatus(item) {
-                let params = new URLSearchParams();
-                params.append("status", item.status);
-                urlPort.post("/responses/" + item.id, params)
-            },
-            downloadSubmission(item) {
-                this.fileBytes = this.submissions.find(function (element, index, array) {
-                    return element.applicantId === item.applicantId
-                }).fileData;
-                download(atob(this.fileBytes), item.applicantName + "submission.zip", "application/zip")
-            }
-        },
-    }
+          }
+        });
+    },
+    methods: {
+      accept(item) {
+        item.status = "INVITED";
+        this.setStatus(item);
+      },
+      reject(item) {
+        item.status = "REJECTED";
+        this.setStatus(item);
+      },
+      route(item) {
+        //todo: сделать линк на резюме пользователя
+      },
+      setStatus(item) {
+        let params = new URLSearchParams();
+        params.append("status", item.status);
+        urlPort.post("/responses/" + item.id, params)
+      },
+      downloadSubmission(item) {
+        this.fileBytes = this.submissions.find(function (element, index, array) {
+          return element.applicantId === item.applicantId
+        }).fileData;
+        download(atob(this.fileBytes), item.applicantName + "submission.zip", "application/zip")
+      }
+    },
+  }
 </script>
 
 <style scoped>
